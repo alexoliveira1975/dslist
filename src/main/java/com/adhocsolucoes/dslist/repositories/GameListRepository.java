29c09717134138aed164ec751704fb9868664df2
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-05-09 21:36:51 -0300

    Get Lists

---
+++ src/main/java/com/adhocsolucoes/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.adhocsolucoes.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.adhocsolucoes.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
